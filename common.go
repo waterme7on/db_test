e39d398437e6a8eee1bc5b9089e26f0b8c1a685b
Jintao Li <watermelonlee@protonmail.com>
2021-10-26 12:01:38 +0800

    feat: add monitor and executor (#2)

---
+++ common.go
@@ -0,0 +1,9 @@
+package main
+
+import "time"
+
+const ResizeInterval = 180 * time.Second
+const MonitorInterval = 5 * time.Second
+const QueryInterval = 5 * time.Second
+const DSN = "http://root@10.77.50.201:31314"
+const PrometheusServerAddr = "http://10.77.50.201:31111"
