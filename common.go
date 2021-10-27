c46493398e4b521455d3e49fa869b32324608003
waterme7on <watermelonlee@protonmail.com>
2021-10-27 17:42:32 +0800

    fix: revise promql

--- common.go
+++ common.go
@@ -10,3 +10,8 @@ const PrometheusServerAddr = "http://10.77.50.201:31111"
 const TestInterval = 1800 * time.Second
 const MaxQuerySize = 10
 const WorkerSize = MaxQuerySize
+
+const (
+	PodMemoryUsagePercentage = "100 * (sum(container_memory_rss{pod=~\"%s.*\"}) by(pod)/1024/1024/1024) / (sum(container_spec_memory_limit_bytes) by(pod)/1024/1024/1024-8)"
+	PodCpuUsagePercentage    = "sum(rate(container_cpu_usage_seconds_total{pod=~\"%s.*\"}[1m])) by (pod) / (((sum(container_spec_cpu_quota) by (pod))/ (sum(container_spec_cpu_period) by (pod))) - 2) * 100"
+)
